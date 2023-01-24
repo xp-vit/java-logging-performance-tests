784cf3c60fe3545fa626c2ad8a09d89eb887d040
Viktar Patotski <xp.vit.blr@gmail.com>
2023-01-24 01:02:27 +0100

    Add Enum Test

---
+++ src/jmh/java/com/patotski/performance/enums/EnumSetVsHashMapBenchmarks.java
@@ -0,0 +1,35 @@
+package com.patotski.performance.enums;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.HashSet;
+
+import static com.patotski.performance.utils.BenchmarkUtils.runBenchmark;
+
+public class EnumSetVsHashMapBenchmarks {
+
+  public enum TestEnum {
+    ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN
+  }
+  @State(Scope.Benchmark)
+  public static class EnumHolder {
+    public HashSet<TestEnum> hashSet = new HashSet<>(Arrays.asList(TestEnum.TWO, TestEnum.FOUR, TestEnum.SIX, TestEnum.EIGHT, TestEnum.TEN));
+    public EnumSet<TestEnum> enumSet = EnumSet.of(TestEnum.TWO, TestEnum.FOUR, TestEnum.SIX, TestEnum.EIGHT, TestEnum.TEN);
+  }
+  @Benchmark
+  public boolean hasSetContains(EnumHolder enumHolder) {
+    return enumHolder.hashSet.contains(TestEnum.TWO);
+  }
+
+  @Benchmark
+  public boolean enumSet(EnumHolder enumHolder) {
+    return enumHolder.enumSet.contains(TestEnum.TWO);
+  }
+  public static void main(String[] args) throws Exception {
+    runBenchmark(EnumSetVsHashMapBenchmarks.class);
+  }
+}
