fe963772663c9b6851146e12d5969b7103a98463
Viktar Patotski <xp.vit.blr@gmail.com>
2023-01-31 00:05:15 +0100

    Addpend Test

---
+++ src/jmh/java/com/patotski/performance/string/StringBuilderBenchmarks.java
@@ -0,0 +1,36 @@
+package com.patotski.performance.string;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+import java.util.regex.Pattern;
+
+import static com.patotski.performance.utils.BenchmarkUtils.runBenchmark;
+
+public class StringBuilderBenchmarks {
+  @State(Scope.Benchmark)
+  public static class InputStringHolder {
+    public StringBuilder sb;
+
+
+    @Setup
+    public void setup() {
+      sb = new StringBuilder();
+    }
+  }
+  @Benchmark
+  public StringBuilder appendChar(InputStringHolder strHolder) {
+    return strHolder.sb.append('a');
+  }
+
+  @Benchmark
+  public StringBuilder appendStr(InputStringHolder strHolder) {
+    return strHolder.sb.append("a");
+  }
+
+  public static void main(String[] args) throws Exception {
+    runBenchmark(StringBuilderBenchmarks.class);
+  }
+}
