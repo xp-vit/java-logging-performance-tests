37092b0e249f114a6bfeeb05781ae6a4aaa30251
Viktar Patotski <xp.vit.blr@gmail.com>
2023-02-25 23:07:08 +0100

    Add Int To String

--- src/jmh/java/com/patotski/performance/string/IntToStringBenchmarks.java
+++ src/jmh/java/com/patotski/performance/string/IntToStringBenchmarks.java
@@ -1,33 +1,32 @@
 package com.patotski.performance.string;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.results.format.ResultFormatType;
-import org.openjdk.jmh.runner.Runner;
-import org.openjdk.jmh.runner.options.Options;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
 import static com.patotski.performance.utils.BenchmarkUtils.runBenchmark;
 
+@State(Scope.Benchmark)
+@BenchmarkMode({Mode.AverageTime})
+@Warmup(iterations = 2, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 3, time = 5, timeUnit = TimeUnit.SECONDS)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Fork(1)
 public class IntToStringBenchmarks {
-    @State(Scope.Benchmark)
-    public static class IntHolder {
-        public int i = 42424242;
-    }
     @Benchmark
-    public String toString(IntHolder mapHolder) {
-        return Integer.toString(mapHolder.i);
+    public String toString() {
+        return Integer.toString(ThreadLocalRandom.current().nextInt());
     }
 
     @Benchmark
-    public String stringConcat(IntHolder mapHolder) {
-        return  "" + mapHolder.i;
+    public String stringConcat() {
+        return  "" + ThreadLocalRandom.current().nextInt();
     }
 
     @Benchmark
-    public String stringValueOf(IntHolder mapHolder) {
-        return  String.valueOf(mapHolder.i);
+    public String stringValueOf() {
+        return  String.valueOf(ThreadLocalRandom.current().nextInt());
     }
 
     public static void main(String[] args) throws Exception {
